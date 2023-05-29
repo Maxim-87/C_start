521a78afa97e3eeed35a89ac58e7e87f8a77c89e
Maxim-87 <maksboks87@mail.ru>
2023-05-29 15:52:19 +0600

    create Run.cs

---
+++ Run.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    interface IRun {
+        float speed { get; set;}
+
+        void RobotRun();
+
+    }
+}
